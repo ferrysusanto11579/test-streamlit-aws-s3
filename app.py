65c1e349e2e2bb4164838fae4f3f4baa3fd32738
ferrysusanto11579 <74810376+ferrysusanto11579@users.noreply.github.com>
2022-12-01 01:54:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import s3fs
 import os
+import pandas as pd
 
 # Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
-# Retrieve file contents.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
 def read_csv(filename):
