51ccae99191f472dc5922a4cfa75379ebe368975
ferrysusanto11579 <74810376+ferrysusanto11579@users.noreply.github.com>
2022-12-01 04:12:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import pandas as pd
 
 # Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-fs = s3fs.S3FileSystem(anon=False, key=os.getenv('AWS_ACCESS_KEY_ID'), secret=os.getenv('AWS_SECRET_ACCESS_KEY'))
-#st.write(os.getenv('AWS_ACCESS_KEY_ID'))
-#st.write(os.getenv('AWS_SECRET_ACCESS_KEY'))
+fs = s3fs.S3FileSystem(anon=False) # , key=os.getenv('AWS_ACCESS_KEY_ID'), secret=os.getenv('AWS_SECRET_ACCESS_KEY')
 
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
@@ -15,6 +13,6 @@ def read_csv(filename):
     with fs.open(filename) as f:
         return pd.read_csv(f)
 
-filepath = 'fsdgbizbucket1/User.csv'
+filepath = 'fsdgbizbucket1/easy-image/database/User.csv'
 users = read_csv(filepath)
 st.dataframe(users)
